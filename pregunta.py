57b915c3faff8aee546fbc8fd2efe4fcf760d221
cpatinore <cpatinore@unal.edu.co>
2022-10-07 11:48:23 -0500

    Inetento 1

--- pregunta.py
+++ pregunta.py
@@ -10,12 +10,51 @@ espacio entre palabra y palabra.
 
 """
 import pandas as pd
+import regex as re
 
 
 def ingest_data():
+    
+    dic={'cluster':[], 'cantidad_de_palabras_clave':[], 'porcentaje_de_palabras_clave':[], 'principales_palabras_clave':[]}
+    columns = ['cluster', 'cantidad_de_palabras_clave', 'porcentaje_de_palabras_clave', 'principales_palabras_clave']
 
-    #
-    # Inserte su código aquí
-    #
+    with open('clusters_report.txt') as report:
+        filas = report.readlines()
+    # No me sirven las 4 primeras filas del txt
+        filas = filas[4:]
+        concatenar=""
 
+        for fila in filas:    
+
+            fila= fila.split()
+
+            #Si la fila esta vacía significa que se haya la siguiente fila    
+            if fila == []:
+                #Se separa cada uno de los patrones
+                aux=re.findall(r"(\s+\d+,\d %)|(\s*\d+)|(\s*([\w+\s]|[/$&+:,;=?@#|'<>^*()%!-])*)",concatenar)
+
+                #Se tomada cada una de las columnas
+                for i in range(0,4):
+                    #Se concatenas los valores dado a que contiene valor ''
+                    valor="".join(aux[i][:-1])
+                    #Si es el porcentaje se elimina los caracteres especiales y se cambia el punto por decimal
+                    if i == 2:
+                        valor= valor.replace(",",".").replace(" %","")
+                    #Si es el texto largo, se elimina el espacio inicial y se organizan las comas para queden palabra, palabra
+                    elif i == 3:
+                        valor= re.sub("^\s","",valor)
+                        valor= re.sub(r"([\w?'<>^*()%!-])(,)(\w)",r"\1\2 \3",valor)
+                    #Se añade el valor al diccionario 
+                    dic[columns[i]].append(valor)
+                #Se reinicia la fila
+                concatenar=""
+            else:
+                #Se concatenan todos los valores de las filas
+                concatenar+= " "+" ".join(fila)
+
+    df=pd.DataFrame(dic)
+    #Se pasan a valores numericos
+    df["cluster"]=pd.to_numeric(df["cluster"])
+    df["cantidad_de_palabras_clave"]=pd.to_numeric(df["cantidad_de_palabras_clave"])
+    df["porcentaje_de_palabras_clave"]= pd.to_numeric(df['porcentaje_de_palabras_clave'])
     return df
